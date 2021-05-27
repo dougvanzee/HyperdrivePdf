c81919c56b2b77ca1fe514d959d83bd08594c549
thevfxguy13 <dmvanzee@gmail.com>
2021-05-26 22:31:21 -0700

    Removed unused namespace

--- Hyperdrive.UI/Views/SideToolbars/PageCountItem.xaml.cs
+++ Hyperdrive.UI/Views/SideToolbars/PageCountItem.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
 using System.Drawing;
-using Windows.UI.Xaml;
 
 namespace Hyperdrive.UI.Views.SideToolbars
 {
