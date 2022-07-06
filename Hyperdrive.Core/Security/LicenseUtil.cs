adda69e22af1c25defc2bdc4104626cf2651d1c7
Doug Van Zee <dmvanzee@gmail.com>
2022-07-06 15:54:35 -0700

    Implemented basics of license util.

--- Hyperdrive.Core/Security/LicenseUtil.cs
+++ Hyperdrive.Core/Security/LicenseUtil.cs
@@ -10,9 +10,10 @@ namespace Hyperdrive.Core.Security
 {
     public class LicenseUtil
     {
-        private bool bLocalLicenseExists = false;
-        private int LicenseCountdown = -1;
-        private int bOnlineLicenseStatus = -1; // -1 = Doesn't exist, 0 = Inactive, 1 = Active 
+        private int LocalLicenseStatus = -1;
+        private int OnlineLicenseStatus = -1; // -1 = Doesn't exist, 0 = Inactive, 1 = Active 
+        private string LocalLicenseFilePath = @"C:\ProgramData\Displace\hyperdrive.lic";
+        private string OnlineLicenseFilePath = "http://www.displace.international/hyperdrive.lic";
 
         public async void StartLicenseCheck()
         {
@@ -30,17 +31,33 @@ namespace Hyperdrive.Core.Security
         {
             CheckLocalLicense();
             CheckOnlineLicense();
+
+            if (OnlineLicenseStatus == 0)
+                Console.WriteLine("License has been deactivated and set local license to 0");
+
+            if (OnlineLicenseStatus == -1 && LocalLicenseStatus == -1)
+                Console.WriteLine("Neither license exists so close program");
+
+            if (OnlineLicenseStatus == 1 && LocalLicenseStatus == -1)
+                CreateLocalLicense();
+
+            if (OnlineLicenseStatus == -1 && LocalLicenseStatus > 0)
+
         }
 
         private void CheckLocalLicense()
         {
-            if (File.Exists(@"C:\ProgramData\Displace\hyperdrive.lic"))
+            if (File.Exists(LocalLicenseFilePath))
             {
                 Console.WriteLine("Local license found");
+                LocalLicenseStatus = Int32.Parse(File.ReadAllText(LocalLicenseFilePath));
+                Console.WriteLine("Local license status: " + LocalLicenseStatus);
             }
             else
             {
                 Console.WriteLine("Local license not found");
+                LocalLicenseStatus = -1;
+                Console.WriteLine("Local license status: " + LocalLicenseStatus);
             }
         }
 
@@ -51,16 +68,43 @@ namespace Hyperdrive.Core.Security
 
             try
             {
-                Stream stream = client.OpenRead("http://www.displace.international/hyperdrive.lic");
+                Stream stream = client.OpenRead(OnlineLicenseFilePath);
                 StreamReader reader = new StreamReader(stream);
                 content = reader.ReadToEnd();
                 Console.WriteLine("Online license found");
+                OnlineLicenseStatus = content == "Active" ?  1 : 0;
+                Console.WriteLine("Online license status: " + OnlineLicenseStatus);
             }
             catch (Exception)
             {
+                OnlineLicenseStatus = -1;
                 Console.WriteLine("Online license not found");
+                Console.WriteLine("Online license status: " + OnlineLicenseStatus);
             }
 
+        }
+
+        private void CreateLocalLicense()
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(LocalLicenseFilePath));
+
+            File.WriteAllText(LocalLicenseFilePath, "6");
+            LocalLicenseStatus = Int32.Parse(File.ReadAllText(LocalLicenseFilePath));
+            Console.WriteLine("Local license status: " + LocalLicenseStatus);
+        }
+
+        private void RunTimeLimitMode()
+        {
+            LocalLicenseStatus--;
+
+            if (LocalLicenseStatus == 0)
+            {
+                Console.WriteLine("License is over and needs to connect to server");
+            }
+            else
+            {
+                Console.WriteLine("Display number of uses left");
+            }
         }
     }
 }
